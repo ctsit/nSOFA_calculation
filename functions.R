9dd0af83509d5b76275923b6901616b2e607e3fc
Laurence James-Woodley <mr.lawjames@gmail.com>
2022-02-10 12:26:06 -0500

    calculate score summaries

--- functions.R
+++ functions.R
@@ -285,9 +285,7 @@ get_nsofa_dataset <- function(create_csv = FALSE) {
                           (number_inotropic_drugs >= 2 & steroids == 0) | 
                             (number_inotropic_drugs == 1 & steroids == 1) ~ 3,
                           number_inotropic_drugs >= 2 & steroids == 1 ~ 4)) %>% 
-    mutate(nsofa_score = platelets + oxygenation + cv) %>% 
-    select(child_mrn_uf,dischg_disposition,child_birth_date, q1hr, inotrope_score,
-           number_inotropic_drugs, oxygenation, platelets, steroids, cv, nsofa_score)
+    mutate(nsofa_score = platelets + oxygenation + cv) 
   
   if (create_csv) {
     write.csv(nsofa, here("output", paste0("nsofa_score_", today(), ".csv")), 
